8bf0352130594dc1b193c31b02049bc9ecf82c92
MartinVillatoros <martinguille17@gmail.com>
2025-03-25 17:18:21 -0600

    sa

--- Factorial.exs
+++ Factorial.exs
@@ -5,3 +5,12 @@ defmodule Calculadora do
     entrada = IO.gets("> ") |> String.trim() |> String.to_integer()
     calcular(entrada)
   end
+
+   def calcular(n) when n < 0 do
+    IO.puts("El número debe ser positivo.")
+  end
+
+  def calcular(n) do
+    resultado = factorial(n)
+    IO.puts("El factorial de #{n} es #{resultado}")
+  end
