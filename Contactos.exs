c8cc3627669803c8009a89e5a3ac1825780333b8
MartinVillatoros <martinguille17@gmail.com>
2025-04-22 07:55:13 -0600

    metodos para los Contactos

--- Contactos.exs
+++ Contactos.exs
@@ -23,3 +23,37 @@ defmodule Agenda do
       _   -> IO.puts("Opción inválida"); menu(contactos)
     end
   end
+
+  def agregar_contacto(contactos) do
+    nombre = IO.gets("Nombre: ") |> String.trim()
+    tel = IO.gets("Teléfono: ") |> String.trim()
+    email = IO.gets("Email: ") |> String.trim()
+    nuevo = %{nombre: nombre, telefono: tel, email: email}
+    [nuevo | contactos]
+  end
+
+  def buscar_contacto(contactos) do
+    nombre = IO.gets("Nombre a buscar: ") |> String.trim()
+    encontrado = Enum.filter(contactos, fn c -> c.nombre == nombre end)
+
+    if encontrado == [] do
+      IO.puts("No se encontró el contacto.")
+    else
+      IO.inspect(encontrado)
+    end
+  end
+
+  def eliminar_contacto(contactos) do
+    nombre = IO.gets("Nombre a eliminar: ") |> String.trim()
+    Enum.reject(contactos, fn c -> c.nombre == nombre end)
+  end
+
+  def mostrar_contactos(contactos) do
+    IO.puts("=== Lista de Contactos ===")
+    Enum.each(contactos, fn c ->
+      IO.puts("Nombre: #{c.nombre}, Tel: #{c.telefono}, Email: #{c.email}")
+    end)
+  end
+end
+
+Agenda.iniciar()
